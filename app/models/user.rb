bd39f8aad623217e5f6d3fa2fc34e38ababd9244
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-11 19:29:57 +0300

    Add validation for new fields then edit register form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :username, presence: true, length: {minimum: 3, maximum: 15}, uniqueness: { case_sensitive: false }
+  validates :fullname, presence: true, length: {minimum: 5, maximum: 35}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
