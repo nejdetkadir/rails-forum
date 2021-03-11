702e5784e8667ec1f722d1deee41ac390542871f
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-12 00:05:03 +0300

    Show topic on the header and create topic details page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_all
+
+  def set_all
+    @topics = Topic.all
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  
+  resources :topic, only: [:show]
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
