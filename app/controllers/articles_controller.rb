f8a98c66a38e14628e58dd9feb7a27001e66fa0b
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-12 20:35:14 +0300

    Show article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
 
   def new
     @article = Article.new
@@ -15,7 +16,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :body, :topic_id)
   end
